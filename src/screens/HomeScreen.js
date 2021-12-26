9cf8bb5f3be5d5773c086fc2af45c44d672b52dc
daya2940 <sauuit2940@gmail.com>
2021-12-27 00:51:21 +0530

    Redux and store setup

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,25 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productAction";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 
 import Product from "../components/Product";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList); //Getting the part of the state where it gets stored using useselector hook.
+
+  const { loading, products, error } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    //This way we are making api call using redux for getting the productlist
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <div>
       <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        ""
+      )}
       <Row>
-        {products.map((product, index) => {
+        {products?.map((product, index) => {
           return (
             <Col sm={12} md={6} lg={4} xl={3} key={index}>
               <Product product={product} />
